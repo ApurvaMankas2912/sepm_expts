bb38b94412dca9d1af0757427d844d1dc4b693b4
ApurvaMankas2912 <145107351+ApurvaMankas2912@users.noreply.github.com>
2025-04-08 09:24:26 +0530

    Update and rename HelloServerlet to HelloServerlet.java

--- src/main/java/com/apurva/HelloServerlet.java
+++ src/main/java/com/apurva/HelloServerlet.java
@@ -4,7 +4,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class HelloServlet extends HttpServlet {
+public class HelloServerlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         response.setContentType("text/html");
